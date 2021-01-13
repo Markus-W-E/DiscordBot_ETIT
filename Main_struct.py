e3384fe4b6d1bc69637ae7d71dc796d02f62f204
Markus-W-E <77384041+Markus-W-E@users.noreply.github.com>
2021-01-13 10:38:51 +0100

    Create Main_struct.py

---
+++ Main_struct.py
@@ -0,0 +1,29 @@
+import discord
+import math
+import os
+import asynio
+import difflib
+import random
+import time
+
+import botcommands
+
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("logged in as" + str(client.user))
+    
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+    
+    try:
+        com = getattr(botcommands, message.content.split()[0][1:])
+        com(message)
+    except Exception as e:
+        raise e
